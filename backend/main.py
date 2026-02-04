60633d3e857bef1ed983e4c427efdc3e6712aa46
nosheii <noshe1997@hotmail.com>
2026-02-04 13:10:30 +0100

    Kommentarer

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from database import init_db, getConnection
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware 
 
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
-init_db()
+app = FastAPI() # Oppretter en FastAPI-applikasjon
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]) # Legger til CORS-middleware for å tillate forespørsler fra alle opprinnelser
+init_db() # Initialiserer databasen ved oppstart av applikasjonen
 
-@app.get("/")
-async def root():
+@app.get("/") # Definerer en GET-endpoint på roten av applikasjonen
+async def root(): # Returnerer en enkel melding for å indikere at serveren kjører
     return {"msg": "You are connected"}
 
 class Pasient(BaseModel): # Definerer en Pydantic-modell for Pasient som brukes til validering av innkommende data
