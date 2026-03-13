4e7b5ae631ec0580d3237b7c3c4a598ca55d3ab0
Aurora__ <enerhaugenaurora12@gmail.com>
2026-03-13 13:06:06 +0100

    Journaldox css settup

--- frontend/src/Journal/Journaldokumenter.jsx
+++ frontend/src/Journal/Journaldokumenter.jsx
@@ -1,6 +1,11 @@
-// rest will becoming soon....
+import { useState } from 'react'
+import { useNavigation } from 'react-router-dom'
+import style from './Journaldokumenter.module.css'
 
 function JournalDokumenter({ fnr }) {
+    
+
+
     return (
         <div style={{ padding: "32px", background: "#ffffff", border: "1.5px solid #d0ecec", borderRadius: "14px" }}>
             <p style={{ color: "#8aabab", fontSize: "15px" }}>JournalDokumenter — kommer snart (fnr: {fnr})</p>
