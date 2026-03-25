544039a92f366ae679ce4cfa34ed8e4c19308f2d
Aurora__ <enerhaugenaurora12@gmail.com>
2026-03-25 13:42:58 +0100

    ompasient, see beskrivelse
    Laget boks til å holde innhold til pasient.
    
    trenger fin pus men er et start punk til å legge in dynamisk innhold som skal kunne endres

--- frontend/src/Journal/Journalompasient.jsx
+++ frontend/src/Journal/Journalompasient.jsx
@@ -1,11 +1,35 @@
-// tomt skjellet bare
+import { useState, useEffect} from "react";
+import styles from './Journalompasient.module.css'
 
 function JournalOmPasient({ fnr }) {
+
+
+
     return (
-        <div style={{ padding: "32px", background: "#ffffff", border: "1.5px solid #d0ecec", borderRadius: "14px" }}>
-            <p style={{ color: "#8aabab", fontSize: "15px" }}>JournalOmPasient — kommer snart (fnr: {fnr})</p>
+        <div className={styles.side}>
+            <div className={styles.kort}>
+                <h2 className = {styles.tittel}><b>Om pasient:</b></h2>
+                <ul>
+                    <li>
+                        Blod type AB+
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.kort}>
+                <h2 className={styles.tittel}><b>Kritisk info:</b></h2>
+                <ul>
+                    <li>
+                        Allergisk mot medisin
+                    </li>
+                    <li>
+                        Hjerte sykdom
+                    </li>
+                </ul>
+            </div>
         </div>
     );
+
 }
+
 
 export default JournalOmPasient;
