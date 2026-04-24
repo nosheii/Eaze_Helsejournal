bd6b0f438ffb3d046dfcf9873067b871104959ec
txzzz56 <mumtazcade8@gmail.com>
2026-04-24 23:36:12 +0200

    Kommentarer

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,13 +1,15 @@
-/* Aurora Krogstad - 261696 */
+/* Aurora Krogstad - 261696 og Mumtaz A. Cade - 273783 */
+// Her defineres navbar-komponenten som brukes gjennom hele applikasjonen. 
+// Inkliuderer logo, navigasjonslenker, og brukerinfo med profilbilde og logg ut-knapp.
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import './Navbar.css'
 import logo from './assets/logo-eaze.png'
 import profilBilde from './assets/profil-bilde.jpg'
 
-function Navbar({ brukerinfo, onLoggUt, rolle }) {
+function Navbar({ brukerinfo, onLoggUt, rolle }) { // Brukerinfo, onLoggUt og rolle kommer som props fra token i App.jsx, og bestemmer hva som vises i navbaren basert på brukerrolle.
   const journalPath = rolle === 'lege' ? '/journal' : `/journal/${brukerinfo.fnr}`
-
+// Her defineres journalPath basert på brukerrolle. Leger skal kunne se journalen til alle pasienter, mens pasienter skal kun se sin egen. 
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -21,7 +23,7 @@ function Navbar({ brukerinfo, onLoggUt, rolle }) {
         </div>
       </div>
       <div className="navbar-right">
-        <span className="navbar-user-name">{brukerinfo.navn}</span>
+        <span className="navbar-user-name">{brukerinfo.navn}</span>  // Her vises navnet til den innloggede brukeren i navbaren, hentet fra brukerinfo.
         <img src={profilBilde} alt="Profil" className="navbar-profile-img" />
         <button className="navbar-loggut-btn" onClick={onLoggUt}>Logg ut</button>
       </div>
